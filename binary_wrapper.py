e6c16e9cfd39837eea514dd42db32e44051a105d
Rafael Monico <rmonico@moninburgo>
2020-08-13 20:20:57 -0300

    Files renamed, __get_attribute__ replaced with __getattr__, tests passing

--- binary_wrapper.py
+++ binary_wrapper.py
@@ -5,36 +5,29 @@ import subprocess
 class BinaryWrapper(object):
 
     def __init__(self, binary):
-        import distutils
+        super().__init__()
+        import distutils.spawn
 
         self._binary_path = distutils.spawn.find_executable(binary)
 
         if not self._binary_path:
             raise BinaryWrapperException('{} not found in PATH'.format(binary))
 
-    def _binary(self, *args):
+    def _call(self, *args):
         process = subprocess.run(
-            [self._git_path] + list(args), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            [self._binary_path] + list(args), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         return process
 
-    def __getattribute__(self, attribute_name):
-        if GitWrapper._should_not_wrap(attribute_name):
-            return object.__getattribute__(self, attribute_name)
-
+    def __getattr__(self, attribute_name):
         def __call_wrapper__(*args, **kwargs):
             command = attribute_name
 
             args = args[1:]
 
-            return self._binary(command, *args, **kwargs)
+            return self._call(command, *args, **kwargs)
 
         return __call_wrapper__
-
-    @staticmethod
-    def _should_not_wrap(attribute_name):
-        return attribute_name.startswith('_')
-
 
 class BinaryWrapperException(Exception):
     pass
---
+++ test_binary_wrapper.py
@@ -0,0 +1,37 @@
+import unittest
+from unittest import TestCase
+
+from binary_wrapper import BinaryWrapper
+
+
+class TestsBinaryWrapper(TestCase):
+
+    def setUp(self):
+        class PythonWrapper(BinaryWrapper):
+            def regular_method(self):
+                return 'regular method called'
+
+            def __init__(self):
+                super().__init__('python3')
+
+            def _call(self, *args):
+                return super()._call('-c', 'import sys; print(";".join(sys.argv[1:]))', *args)
+
+        self._python_wrapper = PythonWrapper()
+
+
+    def test_when_non_existing_method_called_then_pass_method_name_to_binary_as_first_arg(self):
+        process = self._python_wrapper.parameter_passed_to_script()
+        self.assertEqual(process.returncode, 0)
+
+        stdout = process.stdout.decode()[:-1]
+        self.assertEqual(stdout, 'parameter_passed_to_script')
+
+    def test_when_regular_method_called_then_just_call_it(self):
+        result = self._python_wrapper.regular_method()
+
+        self.assertEqual(result, 'regular method called')
+
+
+if __name__ == '__main__':
+    unittest.main()
