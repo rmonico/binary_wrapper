04289b12f30670c70be3e71bca6ec36819cb0e6d
Rafael Monico <rmonico@moninburgo>
2020-08-13 12:12:42 -0300

    BinaryWrapper: first (untested) version

--- binary_wrapper.py
+++ binary_wrapper.py
@@ -2,43 +2,24 @@ import os
 import subprocess
 
 
-# Custom git wrapper should be created by custom factories
+class BinaryWrapper(object):
 
-class GitWrapper(object):
+    def __init__(self, binary):
+        import distutils
 
-    def __init__(self, repository_folder):
-        self._repository_folder = repository_folder
-        self._git_binary = self._get_git_binary()
+        self._binary_path = distutils.spawn.find_executable(binary)
 
-    @staticmethod
-    def _get_git_binary():
-        p = subprocess.run(
-            ['which', 'git'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if not self._binary_path:
+            raise BinaryWrapperException('{} not found in PATH'.format(binary))
 
-        if p.returncode != 0:
-            raise GitWrapperException('git binary not found in PATH')
-
-        output = p.stdout.decode().split('\n')
-
-        return output[0]
-
-    def _git(self, *args):
-        current_directory = os.getcwd()
-
-        os.chdir(self._repository_folder)
-
+    def _binary(self, *args):
         process = subprocess.run(
-            [self._git_binary] + list(args), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        os.chdir(current_directory)
+            [self._git_path] + list(args), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         return process
 
-    def is_repository(self):
-        return self._git('status').returncode == 0
-
     def __getattribute__(self, attribute_name):
-        if GitWrapper._should_wrap_attribute(attribute_name):
+        if GitWrapper._should_not_wrap(attribute_name):
             return object.__getattribute__(self, attribute_name)
 
         def __call_wrapper__(*args, **kwargs):
@@ -46,14 +27,14 @@ class GitWrapper(object):
 
             args = args[1:]
 
-            return self._git(command, *args, **kwargs)
+            return self._binary(command, *args, **kwargs)
 
         return __call_wrapper__
 
     @staticmethod
-    def _should_wrap_attribute(attribute_name):
+    def _should_not_wrap(attribute_name):
         return attribute_name.startswith('_')
 
 
-class GitWrapperException(Exception):
+class BinaryWrapperException(Exception):
     pass
