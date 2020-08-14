a0b197c7b314b4c7d9f73df1f0ec39530d42b10c
Rafael Monico <rmonico@moninburgo>
2020-08-14 17:33:17 -0300

    Exporting main package

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 from setuptools import setup
 
 setup(name='binary_wrapper',
-      version='0.1',
+      version='0.2',
       description='A generic binary wrapper',
       url='https://github.com/rmonico/binary_wrapper',
       author='Rafael Monico',
       author_email='rmonico1@gmail.com',
       license='GPL3',
+      packages=['.'],
       include_package_data=True,
       zip_safe=False)
