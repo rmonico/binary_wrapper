d1c4020f39bd18cb6c6d3356dec4facbad5a445d
Rafael Monico <rmonico@moninburgo>
2020-08-13 22:17:59 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name='binary_wrapper',
+      version='0.1',
+      description='A generic binary wrapper',
+      url='https://github.com/rmonico/binary_wrapper',
+      author='Rafael Monico',
+      author_email='rmonico1@gmail.com',
+      license='GPL3',
+      include_package_data=True,
+      },
+      zip_safe=False, install_requires=['PyYAML'])
